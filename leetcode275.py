9ce9d2760ca1259dae885dc16d4f7419e9b574b8
kduan005 <dk07071@hotmail.com>
2018-12-13 22:53:11 -0600

    H-Index II

--- leetcode275.py
+++ leetcode275.py
@@ -7,14 +7,11 @@ class Solution(object):
         if not citations: return 0
         n = len(citations)
         l, r = 0, n-1
-        while l < r:
+        while l <= r:
             mid = l + (r-l)/2
-            if citations[mid] < n - mid:
-                l = mid + 1
+            if citations[mid] >= n - mid:
+                r = mid - 1
             else:
-                r = mid
-        if citations[l] >= n-l:
-            return n-l
-        else:
-            return 0
+                l = mid + 1
+        return n-l
         
