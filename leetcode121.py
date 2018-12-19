8183e1b3c6047355da8d3b225acacbe721cda064
kduan005 <dk07071@hotmail.com>
2018-12-18 23:01:52 -0600

    Best Time to Buy and Sell Stock

---
+++ leetcode121.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        H, N = float('-inf'), 0
+        for price in prices:
+            H, N = max(H, -price), max(N, H + price)
+        return N
