bdd0e5c3c5ea0d6ce1fd5fcf1e66d089a820d162
kduan005 <dk07071@hotmail.com>
2018-12-20 17:33:01 -0600

    Maximum Sum Circular Subarray

--- leetcode918.py
+++ leetcode918.py
@@ -4,12 +4,11 @@ class Solution(object):
         :type A: List[int]
         :rtype: int
         """
-        dpmin, dpmax = [0 for i in range(len(A))], [0 for i in range(len(A))]
-        dpmin[0], dpmax[0] = A[0], A[0]
+        dpmin, dpmax = A[0], A[0]
         minsub, maxsub = A[0], A[0]
         for i in range(1, len(A)):
-            dpmin[i] = A[i] if dpmin[i-1] > 0 else dpmin[i-1] + A[i]
-            minsub = min(minsub, dpmin[i])
-            dpmax[i] = A[i] if dpmax[i-1] < 0 else dpmax[i-1] + A[i]
-            maxsub = max(maxsub, dpmax[i])
+            dpmin = A[i] if dpmin > 0 else dpmin + A[i]
+            minsub = min(minsub, dpmin)
+            dpmax = A[i] if dpmax < 0 else dpmax + A[i]
+            maxsub = max(maxsub, dpmax)
         return maxsub if maxsub < 0 else max(maxsub, sum(A) - minsub)
