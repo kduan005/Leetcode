770812d481e4814a0c4d52e506c5bd379fc75e81
kduan005 <dk07071@hotmail.com>
2018-12-23 22:53:44 -0600

    Single Number II

---
+++ leetcode137.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        x1, x2 = 0, 0
+        for num in nums:
+            x2 ^= x1 & num
+            x1 ^= num
+            mask = ~(x1 & x2)
+            x2 &= mask
+            x1 &= mask
+        return x1
